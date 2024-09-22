044e03d9a7c36b86c5839eff5049c9d9287f2dc1
sajibkhandev <sajibkhan.mern@gmail.com>
2024-09-22 14:53:41 +0600

    banner done

--- src/Layouts/Banner.jsx
+++ src/Layouts/Banner.jsx
@@ -3,6 +3,7 @@ import Container from '../components/Container'
 import Flex from '../components/Flex'
 import BannerImages from '../assets/banner.png'
 import Button from '../components/Button'
+import { FaArrowRightLong } from "react-icons/fa6";
 
 const Banner = () => {
   return (
@@ -10,11 +11,11 @@ const Banner = () => {
         <Container>
             <Flex>
                 <div className='w-[50%]'>
-                    <h1 className='text-3xl'>Building stellar websites for early startups</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
-                    <div className='flex'>
+                    <h1 className='text-[54px] text-[#fff] font-pop font-semibold w-[582px]'>Building stellar websites for early startups</h1>
+                    <p className='text-base text-[#fff] font-pop font-normal w-[562px] pt-6 pb-12'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+                    <div className='flex relative'>
                         <Button text='View our work'/>
-                        <Button text='View our work'/>
+                        <div className='flex items-center justify-center text-[#fff] absolute top-[18px] left-[256px] group'><a  href="">View Pricing</a><FaArrowRightLong className='absolute top-[6px] left-[100px] opacity-0 group-hover:opacity-100'/></div>
                     </div>
                 </div>
                 <div className='w-[50%]'>
